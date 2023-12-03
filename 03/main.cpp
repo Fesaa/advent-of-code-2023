4718c323e5bca0bc385477bc0889057f4f80f73f
Amelia <77553571+Fesaa@users.noreply.github.com>
2023-12-03 15:49:24 +0100

    Day 3 - part 2

--- 03/main.cpp
+++ 03/main.cpp
@@ -9,7 +9,9 @@ bool isSymbol(char c) { return !isdigit(c) && c != '.'; }
 bool validDigit(int rowIndex, int startIndex, int endIndex,
                 std::vector<std::vector<char>> &grid);
 
+int gearProduct(int row, int column, std::vector<std::vector<char>> grid);
 int part1(std::vector<std::vector<char>> grid);
+int part2(std::vector<std::vector<char>> grid);
 
 int main() {
   std::ifstream inputFile("input.txt");
@@ -36,9 +38,126 @@ int main() {
   inputFile.close();
 
   int sum = part1(grid);
+  int sum2 = part2(grid);
   printf("Sum: %d\n", sum);
+  printf("Sum2: %d\n", sum2);
 
   return 0;
+}
+
+int min(int a, int b) {
+  if (a < b) {
+    return a;
+  }
+  return b;
+}
+
+int max(int a, int b) {
+  if (a > b) {
+    return a;
+  }
+  return b;
+}
+
+bool isNeighbour(int start, int end, int house) {
+  if (start == house || end == house) {
+    return true;
+  }
+
+  if (start < house && end > house) {
+    return true;
+  }
+
+  if (start == house + 1) {
+    return true;
+  }
+
+  if (end == house - 1) {
+    return true;
+  }
+
+  return false;
+}
+
+int gearProduct(int rowIndex, int columnIndex,
+                std::vector<std::vector<char>> grid) {
+
+  int prod = 1;
+  int adders = 0;
+
+  std::vector<std::vector<char>> rows = {};
+  if (rowIndex > 0) {
+    rows.push_back(grid[rowIndex - 1]);
+  }
+
+  if (rowIndex < grid.size() - 1) {
+    rows.push_back(grid[rowIndex + 1]);
+  }
+
+  rows.push_back(grid[rowIndex]);
+
+  for (std::vector<char> row : rows) {
+    int start = max(columnIndex - 3, 0);
+    int end = min(columnIndex + 3, row.size() - 1);
+
+    int startIndex = -1;
+    int endIndex = -1;
+    int digit = 0;
+
+    for (int pos = start; pos <= end; pos++) {
+      char c = row[pos];
+      if (isdigit(c)) {
+        digit = digit * 10 + (c - '0');
+        if (startIndex == -1) {
+          startIndex = pos;
+        } else {
+          endIndex = pos;
+        }
+      } else if (digit != 0) {
+        if (endIndex == -1) {
+          endIndex = startIndex;
+        }
+
+        if (isNeighbour(startIndex, endIndex, columnIndex)) {
+          prod *= digit;
+          adders++;
+        }
+
+        digit = 0;
+        startIndex = -1;
+        endIndex = -1;
+      }
+    }
+
+    if (isNeighbour(startIndex, endIndex, columnIndex)) {
+      prod *= digit;
+      adders++;
+    }
+  }
+
+  if (adders != 2) {
+    return 0;
+  }
+
+  return prod;
+}
+
+int part2(std::vector<std::vector<char>> grid) {
+  int sum = 0;
+
+  for (int i = 0; i < grid.size(); i++) {
+    std::vector<char> row = grid[i];
+
+    for (int j = 0; j < row.size(); j++) {
+      char c = row[j];
+
+      if (c == '*') {
+        sum += gearProduct(i, j, grid);
+      }
+    }
+  }
+
+  return sum;
 }
 
 int part1(std::vector<std::vector<char>> grid) {
