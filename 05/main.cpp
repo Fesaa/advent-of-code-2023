eb076c46a4ffe2118baa5a22ce15724ade306869
Amelia <77553571+Fesaa@users.noreply.github.com>
2023-12-05 15:26:42 +0100

    Day 5 - part 2

--- 05/main.cpp
+++ 05/main.cpp
@@ -1,12 +1,19 @@
 #include <cstdio>
 #include <fstream>
 #include <limits>
+#include <queue>
 #include <vector>
 
+using std::queue;
 using std::string;
 using std::vector;
 
 vector<long> extractNumbers(string s, string delimiter);
+
+struct Range {
+  long start;
+  long end;
+};
 
 struct RangeConv {
   long source_start;
@@ -21,12 +28,82 @@ struct RangeConv {
     long diff = source - source_start;
     return dest_start + diff;
   }
+
+  long source_end() { return source_start + range; }
+  long dest_end() { return dest_start + range; }
 };
 
 struct Mapper {
   vector<RangeConv> convertors = {};
 
   void addRangeConv(RangeConv range) { convertors.push_back(range); }
+
+  vector<Range> map(Range range) {
+    vector<Range> ranges = {};
+    queue<Range> rangeQueue;
+    rangeQueue.push(range);
+
+    while (!rangeQueue.empty()) {
+      Range cur = rangeQueue.front();
+      int curSize = ranges.size();
+      rangeQueue.pop();
+      for (RangeConv conv : convertors) {
+        // No overlap
+        if (cur.end < conv.source_start || cur.start > conv.source_end()) {
+          continue;
+        }
+
+        // Full overlap
+        if (cur.start >= conv.source_start && cur.end <= conv.source_end()) {
+          Range r =
+              Range{.start = conv.map(cur.start), .end = conv.map(cur.end)};
+          ranges.push_back(r);
+          break;
+        }
+
+        // Partial overlap
+
+        if (cur.start <= conv.source_start && cur.end >= conv.source_end()) {
+          Range convSize =
+              Range{.start = conv.dest_start, .end = conv.dest_end()};
+          Range leftSize =
+              Range{.start = cur.start, .end = conv.source_start - 1};
+          Range rightSize =
+              Range{.start = conv.source_end() + 1, .end = cur.end};
+          ranges.push_back(convSize);
+          rangeQueue.push(leftSize);
+          rangeQueue.push(rightSize);
+          break;
+        }
+
+        // End poking out
+        if (cur.start >= conv.source_start) {
+          Range overlap =
+              Range{.start = conv.map(cur.start), .end = conv.dest_end()};
+          Range pokingOut =
+              Range{.start = conv.source_end() + 1, .end = cur.end};
+          ranges.push_back(overlap);
+          rangeQueue.push(pokingOut);
+          break;
+        }
+
+        // Start poking out
+        Range overlap =
+            Range{.start = conv.dest_start, .end = conv.map(cur.end)};
+        Range pokingOut =
+            Range{.start = cur.start, .end = conv.source_start - 1};
+        ranges.push_back(overlap);
+        rangeQueue.push(pokingOut);
+        break;
+      }
+
+      if (ranges.size() == curSize) {
+        ranges.push_back(cur);
+      }
+    }
+
+    return ranges;
+  }
 
   long map(long source) {
     long res = source;
@@ -41,6 +118,7 @@ struct Mapper {
 };
 
 long part1(vector<long> seeds, vector<Mapper *> mappers);
+long part2(vector<long> seeds, vector<Mapper *> mappers);
 
 int main() {
   std ::ifstream inputFile("input.txt");
@@ -91,9 +169,42 @@ int main() {
     mappers.push_back(cur);
   }
 
-  printf("Min seed: %ld\n", part1(seeds, mappers));
+  printf("part1: %ld\n", part1(seeds, mappers));
+  printf("part2: %ld\n", part2(seeds, mappers));
 
   return 0;
+}
+
+long part2(vector<long> seeds, vector<Mapper *> mappers) {
+  vector<Range> ranges;
+  long last = 0;
+  for (int i = 0; i < seeds.size(); i++) {
+    if (i % 2 == 1) {
+      ranges.push_back(Range{.start = last, .end = last + seeds[i]});
+    } else {
+      last = seeds[i];
+    }
+  }
+
+  long minmap = std::numeric_limits<long>::max();
+  for (Range range : ranges) {
+    vector<Range> conv;
+    conv.push_back(range);
+    for (Mapper *map : mappers) {
+      vector<Range> mapConv;
+      for (Range cur : conv) {
+        vector<Range> convCur = map->map(cur);
+        mapConv.insert(mapConv.end(), convCur.begin(), convCur.end());
+      }
+      conv = mapConv;
+    }
+    for (Range r : conv) {
+      minmap = std::min(minmap, r.start);
+      minmap = std::min(minmap, r.end);
+    }
+  }
+
+  return minmap;
 }
 
 long part1(vector<long> seeds, vector<Mapper *> mappers) {
