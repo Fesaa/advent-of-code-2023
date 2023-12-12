f1f1869eb55083de68f462aa67fb45954d98778e
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-12-12 20:55:19 +0100

    Part 2

--- 12/main.py
+++ 12/main.py
@@ -1,14 +1,26 @@
 with open("input") as f:
-    input = f.read().strip().split("\n")
+    uinput = f.read().strip().split("\n")
 
 
 records: list[tuple[str, list[int]]] = []
-for line in input:
+records2: list[tuple[str, list[int]]] = []
+for line in uinput:
     p = line.split(" ")
     c = list(map(int, p[1].split(",")))
     records.append((p[0], c))
 
+    c2 = []
+    for _ in range(5):
+        for i in c:
+            c2.append(i)
+    d = p[0]
+    records2.append(("?".join((d, d, d, d, d)), c2))
+
+cache: dict[str, int] = {}
 def permutations(s: str, counts: list[int]) -> int:
+    if s + ";".join(map(str, counts)) in cache:
+        return cache[s + ";".join(map(str, counts))]
+
     if s == "":
         return 1 if len(counts) == 0 else 0
 
@@ -31,7 +43,10 @@ def permutations(s: str, counts: list[int]) -> int:
             perms += permutations("", counts[1:])
         else:
             perms += permutations(s[counts[0]+1:], counts[1:])
+    cache[s + ";".join(map(str, counts))] = perms
     return perms
 
-print("Part 2:", sum(permutations(r[0], r[1]) for r in records), "valid messages")
+print("Part 1:", sum(permutations(r[0], r[1]) for r in records))
+print("Part 2:", sum(permutations(r[0], r[1]) for r in records2))
+
 
